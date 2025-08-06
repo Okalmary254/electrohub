858fae80db606bffb2bb9c8bc1539593ceb7184d
Fidel <fideleliudclimax@gmail.com>
2025-08-07 00:05:54 +0300

    new proj

--- shop/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    # ... other urls
-    path('signout/', views.signout, name='signout'),
-    path('product/<int:pk>/', views.product_detail, name='product_detail'),
-]
