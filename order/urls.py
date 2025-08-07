bf53e1872db860c28e4133f21b2129c996fe8372
okalmary254 <okalmohn18@gmail.com>
2025-08-07 22:42:06 +0300

    created urls for order

--- order/urls.py
+++ order/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-# from . import views  # Uncomment and use if you have order views
+from . import views
 
 app_name = "order"
 
 urlpatterns = [
-    # Example:
-    # path('', views.order_list, name='list'),
-    # path('<int:pk>/', views.order_detail, name='detail'),
+    path('', views.order_list, name='list'),
+    path('<int:pk>/', views.order_detail, name='detail'),
 ]
