4f76b07950ab223a64478c8188dbc9816bff3f06
okalmary254 <okalmohn18@gmail.com>
2025-08-11 18:28:54 +0300

    implemented cart, urls for cart and brand products modified

--- cart/urls.py
+++ cart/urls.py
@@ -1,16 +1,16 @@
 from django.urls import path
 from . import views
 
-app_name = 'cart'
+app_name = "cart"
 
 urlpatterns = [
     # Cart pages
     path('', views.cart_detail, name='detail'),
     
     # Cart actions
-    path('add/', views.add_to_cart, name='add'),
+    path('add/<int:product_id>/', views.add_to_cart, name='add'),
     path('update/', views.update_cart_item, name='update'),
-    path('remove/', views.remove_from_cart, name='remove'),
+    path('remove/<int:product_id>/', views.remove_from_cart, name='remove'),
     path('clear/', views.clear_cart, name='clear'),
     
     # Ajax endpoints
